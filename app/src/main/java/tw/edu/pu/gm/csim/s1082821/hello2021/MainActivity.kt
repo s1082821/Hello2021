b8d5c894444ee907dd1c3c11d641d9205d025342
s1082821 <s1082821@gm.pu.edu.tw>
2021-03-12 15:55:21 +0800

    第二次commit (修改文字元件)

--- app/src/main/java/tw/edu/pu/gm/csim/s1082821/hello2021/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/gm/csim/s1082821/hello2021/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Hello tcyang"
+
     }
 }
